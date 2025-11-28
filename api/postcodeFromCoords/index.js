effe3d3759fd6feeaeb0093aa9098c6212614756
Nick <nickadkinson@hotmail.co.uk>
2025-11-28 10:19:22 +0000

    added additional error logging

--- api/postcodeFromCoords/index.js
+++ api/postcodeFromCoords/index.js
@@ -2,15 +2,19 @@ import { reverseGeocodeToPostcode } from '../_lib/locationCore.js';
 
 export default async function (context, req) {
     try {
+        context.log.info('Raw request body:', req.body);
         const { latitude, longitude } = req.body || {};
         context.log.info('Received request for reverse geocoding:', { latitude, longitude });
+        if (typeof latitude !== 'number' || typeof longitude !== 'number') {
+            throw new Error('Latitude and longitude must be numbers. Received: ' + JSON.stringify({ latitude, longitude }));
+        }
         const result = await reverseGeocodeToPostcode(latitude, longitude);
         context.log.info('Reverse geocoding result:', result);
         context.res = { status: 200, body: { postcode: result.postcode } };
     } catch (err) {
         context.log.error('Reverse geocoding error:', err);
         context.res = {
-            status: 400,
+            status: 500,
             body: {
                 error: 'reverse_geocode_failed',
                 detail: err.message,
