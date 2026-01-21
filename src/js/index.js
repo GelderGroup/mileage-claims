86341aa5c0695feadcb627468257336cc23b0f75
Nick <nickadkinson@hotmail.co.uk>
2026-01-21 14:41:59 +0000

    change favison according to theme

--- src/js/index.js
+++ src/js/index.js
@@ -5,20 +5,25 @@ import { MoonIcon, SunIcon } from './utils/icons.js';
 function initializeTheme() {
     const html = document.documentElement;
     const btn = document.getElementById('theme-btn');
+    const favIconLink = document.getElementById('fav-icon-link');
+
     const moonIcon = MoonIcon();
     const sunIcon = SunIcon();
 
-    const setIcon = () => setChildren(btn, html.dataset.theme === 'dark' ? moonIcon : sunIcon);
+    const setThemeIcon = () => setChildren(btn, html.dataset.theme === 'dark' ? moonIcon : sunIcon);
+    const setFavicon = () => favIconLink.href = html.dataset.theme === 'dark' ? 'lib/assets/favicon-light.svg' : 'lib/assets/favicon-dark.svg';
 
     btn.addEventListener('click', () => {
         html.dataset.theme = html.dataset.theme === 'dark' ? 'light' : 'dark';
         localStorage.setItem('ts-theme', html.dataset.theme);
-        setIcon();
+        setThemeIcon();
+        setFavicon();
     });
 
     // Set initial theme
     html.dataset.theme = localStorage.getItem('ts-theme') || 'dark';
-    setIcon();
+    setThemeIcon();
+    setFavicon();
 }
 
 initializeTheme();
