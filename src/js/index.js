e90bf1cf4a93411ee52a0f5b4be762a2aa48e583
Nick <nickadkinson@hotmail.co.uk>
2026-01-21 19:32:58 +0000

    change icons for consistent height

--- src/js/index.js
+++ src/js/index.js
@@ -1,29 +1,25 @@
 import { mount, setChildren } from 'redom';
 import App from "./App";
-import { MoonIcon, SunIcon } from './utils/icons.js';
+import { MoonSolidIcon, SunSolidIcon } from './utils/icons.js';
 
 function initializeTheme() {
     const html = document.documentElement;
     const btn = document.getElementById('theme-btn');
-    const favIconLink = document.getElementById('fav-icon-link');
 
-    const moonIcon = MoonIcon();
-    const sunIcon = SunIcon();
+    const moonIcon = MoonSolidIcon();
+    const sunIcon = SunSolidIcon();
 
     const setThemeIcon = () => setChildren(btn, html.dataset.theme === 'dark' ? moonIcon : sunIcon);
-    const setFavicon = () => favIconLink.href = html.dataset.theme === 'dark' ? 'lib/assets/favicon-light.svg' : 'lib/assets/favicon-dark.svg';
 
     btn.addEventListener('click', () => {
         html.dataset.theme = html.dataset.theme === 'dark' ? 'light' : 'dark';
         localStorage.setItem('ts-theme', html.dataset.theme);
         setThemeIcon();
-        // setFavicon();
     });
 
     // Set initial theme
     html.dataset.theme = localStorage.getItem('ts-theme') || 'dark';
     setThemeIcon();
-    // setFavicon();
 }
 
 initializeTheme();
